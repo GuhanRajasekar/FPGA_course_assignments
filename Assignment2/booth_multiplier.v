db2d47c0f4d4b70ed0e66f2b90bb446a8128d73b
GuhanRajasekar <149869855+GuhanRajasekar@users.noreply.github.com>
2024-02-25 18:38:59 +0530

    Final working copy
    Updates made:
    1). Clock signal has been added.
    2). Loading the inputs and writing the outputs is done at the positive edge of the clock signal.

--- Assignment2/booth_multiplier.v
+++ Assignment2/booth_multiplier.v
@@ -26,30 +26,48 @@ always@(*)
 endmodule
 
 
-module booth_multiplier(x,y,z);
+module booth_multiplier(x,y,clk,rst,result,carry);
   input  [7:0]  x;   // register to store the multiplicand
   input  [7:0]  y;   // register to store the multiplier
-  output reg [15:0] z;   // register to store the final result
+  input clk,rst;     //
+  output reg [15:0] result;   // register to store the final result
+  output reg carry;
   
   // 8 bit wires to denote the temporary partial products
   // These partial products do not have sign extension or zero padding at LSB
   wire [7:0] pp0_temp , pp1_temp , pp2_temp , pp3_temp;
   
-  //module instantiations to generate the partial products without sign extension at the MSB and zero padding at the LSB
-  genPP g (.a({y[1:0],1'b0}) , .x(x) , .pp(pp0_temp));
-  genPP u (.a( y[3:1]   )    , .x(x) , .pp(pp1_temp));
-  genPP h (.a( y[5:3]   )    , .x(x) , .pp(pp2_temp));
-  genPP n (.a( y[7:5]   )    , .x(x) , .pp(pp3_temp));
-   
-  // 15 bit register to store the complete partial products with sign extension at MSB and zero padding at the LSB
-  reg [15:0] pp0, pp1,pp2,pp3;
+  reg [7:0] x_reg,y_reg;       // 8 bit registers to hold the mulitplicand and the multiplier values
+  reg [15:0] pp0, pp1,pp2,pp3; // 16 bit register to store the complete partial products with sign extension at MSB and zero padding at the LSB
   
+  //module instantiations to generate the partial products without sign extension at the MSB and zero padding at the LSB
+  genPP G (.a({y_reg[1:0],1'b0}) , .x(x_reg) , .pp(pp0_temp));
+  genPP U (.a( y_reg[3:1]   )    , .x(x_reg) , .pp(pp1_temp));
+  genPP H (.a( y_reg[5:3]   )    , .x(x_reg) , .pp(pp2_temp));
+  genPP A (.a( y_reg[7:5]   )    , .x(x_reg) , .pp(pp3_temp));
+  
+  always@(posedge clk)
+    begin
+       if(rst == 0)               // Reset all the input and output registers
+         begin
+           x_reg   <= 8'b0;
+           y_reg   <= 8'b0;
+           result  <= 16'b0;
+           carry   <= 1'b0;
+         end
+       else                      // Load all the input and the output registers with appropriate values
+         begin
+            x_reg <= x;
+            y_reg <= y;
+            {carry , result} <= (pp0 + pp1 + pp2 + pp3);          
+         end
+    end
+    
   always@(*)
      begin
          pp0 = { {8{pp0_temp[7]}} , pp0_temp};                  // 8 bits at MSB for sign extension and zero bits at LSB for zero padding
          pp1 = { {6{pp1_temp[7]}} , pp1_temp , {2{1'b0}}};      // 6 bits at MSB for sign extension and two  bits at LSB for zero padding
          pp2 = { {4{pp2_temp[7]}} , pp2_temp , {4{1'b0}}};      // 4 bits at MSB for sign extension and four bits at LSB for zero padding
          pp3 = { {2{pp3_temp[7]}} , pp3_temp , {6{1'b0}}};      // 2 bits at MSB for sign extension and six  bits at LSB for zero padding
-         z   = pp0 + pp1 + pp2 + pp3;                           // adding all the partial products to get the final result
      end
 endmodule 
--- Assignment2/booth_multiplier_tb.v
+++ Assignment2/booth_multiplier_tb.v
@@ -1,12 +1,19 @@
 module booth_multiplier_tb;
  reg    [7:0] x,y;    // Inputs in the test bench must be registers as they are given values inside the initial block
- wire   [15:0] z;     // To denote the sum of all the partial products  
- booth_multiplier a(.x(x),.y(y),.z(z));
+ reg    clk, rst;     // Register inputs for clock and reset
+ wire   [15:0] result;     // To denote the sum of all the partial products  
+ wire   carry;
+ booth_multiplier a(.x(x),.y(y),.clk(clk),.rst(rst),.result(result),.carry(carry));
  
+ always 
+    begin
+       #2 clk = ~clk;
+    end
  initial 
    begin
-      $monitor(" time =%d , x = %b , y = %b, z = %b",$time,x,y,z);
-      #5  x = 8'b11010011; y = 8'b00001001;
+      # 0 clk = 1'b0;  rst = 1'b0;
+      $monitor(" time =%d , x = %b , y = %b, result = %b , carry = %b",$time,x,y,result,carry);
+      #1  rst = 1'b1; x = 8'b00001101; y = 8'b10010111;
       #10 $finish;
    end
 
