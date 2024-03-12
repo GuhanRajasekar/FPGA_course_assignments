7f9804b5fa6c042982e92b0d164204fcba4b429e
GuhanRajasekar <149869855+GuhanRajasekar@users.noreply.github.com>
2024-03-12 19:42:51 +0530

    Improved Unpipelined Booth multiplier with higher operational frequency
    The clock frequency in the un-pipelined booth multiplier has been increased from 100Mhz to roughly 150Mhz.
    
    This is because in the earlier case, zero padding of the partial products was done one by one. This is un-necessary as we can do them all the same time. So this has resulted in improved clock frequency.

--- Assignment2/Guhan_22410_FPGA_Assignment2/improved_booth_multiplier.v
+++ Assignment2/Guhan_22410_FPGA_Assignment2/improved_booth_multiplier.v
@@ -1,3 +1,10 @@
+// This is the code of improved booth multiplier where the setup slack is 3.47 ns 
+// Hence min Tclk = 10n - 3.47n = 6.53ns
+// So the max clock frequency is 1/6.53n = 153.139MHz.
+// Here the idea is use the zero padding of the partial products at the same time rather than doing them one by one.
+// This has resulted in  increased clock frequency. 
+
+
 `timescale 1ns/1ps
 module genPP(a,x,pp);
 input  [2:0]   a;     // register to store 3 bits of the mutiplicand based on which Partial product is generated
@@ -16,7 +23,7 @@ always@(*)
        3'b100:
           begin
                temp = {x[7],x,1'b0};   // 2x
-               pp   = -temp;           // -2x ('-' in verilog will take 2's complement)
+               pp   = -temp;           // -2x ('-' in veriilog will take 2's complement)
           end
        3'b101:
           begin
@@ -36,12 +43,12 @@ always@(*)
 endmodule
 
 
-module booth_multiplier(x,y,clk,rst,result,carry);
+module booth_multiplier(x,y,clk,rst,result_reg,carry_reg);
   input [7:0]  x;    // register to store the multiplicand
   input [7:0]  y;    // register to store the multiplier
   input clk,rst;     // wires that are driven by clk and reset signals
-  output reg  [15:0] result;   // register to store the final result
-  output reg  carry;
+  output reg  [15:0] result_reg;   // register to store the final result
+  output reg  carry_reg;           // register to store the carry of the final result
   
   // 10 bit wires to denote the temporary partial products
   // Having it as 10 bits will help us avoid losing the MSB with Left Shifting
@@ -50,6 +57,9 @@ module booth_multiplier(x,y,clk,rst,result,carry);
   
   reg [7:0] x_reg,y_reg;       // 8 bit registers to hold the mulitplicand and the multiplier values
   reg [15:0] pp0, pp1,pp2,pp3; // 16 bit register to store the complete partial products with sign extension at MSB and zero padding at the LSB
+  reg [15:0] result_comb;   // register to store the result. Computation done in combinational always block
+  reg carry_comb; // register to store the carry. Computation done in combinational always block
+  
   
   //module instantiations to generate the partial products without sign extension at the MSB and zero padding at the LSB
   genPP G (.a({y_reg[1:0],1'b0}) , .x(x_reg) , .pp(pp0_temp));
@@ -63,22 +73,25 @@ module booth_multiplier(x,y,clk,rst,result,carry);
          begin
            x_reg   <= 8'b0;
            y_reg   <= 8'b0;
-           result  <= 16'b0;
-           carry   <= 1'b0;
+           result_reg  <= 16'b0;
+           carry_reg   <= 1'b0;
          end
        else                      // Load all the input and the output registers with appropriate values
          begin
             x_reg <= x;
             y_reg <= y;
-            {carry , result} <= (pp0 + pp1 + pp2 + pp3);          
+           {carry_reg , result_reg} <= {carry_comb,result_comb};          
          end
     end
-    
-  always@(*)
-     begin
-         pp0 = { {6{pp0_temp[9]}} , pp0_temp};                  // 6 bits at MSB for sign extension and zero bits at LSB for zero padding
-         pp1 = { {4{pp1_temp[9]}} , pp1_temp , {2{1'b0}}};      // 4 bits at MSB for sign extension and two  bits at LSB for zero padding
-         pp2 = { {2{pp2_temp[9]}} , pp2_temp , {4{1'b0}}};      // 2 bits at MSB for sign extension and four bits at LSB for zero padding
-         pp3 = { pp3_temp , {6{1'b0}}};                         // six  bits at LSB for zero padding
-     end
+  
+  // Since all the partial products can be computed parallely, we have used separate always blocks here.
+  // all the always blocks will begin execution at time t = 0
+  // So the computation of partial products will take place in a parallel manner. 
+  // The following set of changes has helped us improve the clock frequency from 100MHz to roughly 150MHz.
+  
+  always@(*)  pp0 = { {6{pp0_temp[9]}} , pp0_temp};                  // 6 bits at MSB for sign extension and zero bits at LSB for zero padding
+  always@(*)  pp1 = { {4{pp1_temp[9]}} , pp1_temp , {2{1'b0}}};      // 4 bits at MSB for sign extension and two  bits at LSB for zero padding
+  always@(*)  pp2 = { {2{pp2_temp[9]}} , pp2_temp , {4{1'b0}}};      // 2 bits at MSB for sign extension and four bits at LSB for zero padding
+  always@(*)  pp3 = { pp3_temp , {6{1'b0}}};                         // six  bits at LSB for zero padding
+  always@(*)  {carry_comb,result_comb} = pp0 + pp1+pp2+pp3;          // adding and saving the result
 endmodule 
---
+++ Assignment2/Guhan_22410_FPGA_Assignment2/improved_booth_multiplier_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns/1ps
+module booth_multiplier_tb;
+ reg    [7:0] x,y;    // Inputs in the test bench must be registers as they are given values inside the initial block
+ reg    clk, rst;     // Register inputs for clock and reset
+ wire   [15:0] result_reg;     // To denote the sum of all the partial products  
+ wire   carry_reg;
+ booth_multiplier a(.x(x),.y(y),.clk(clk),.rst(rst),.result_reg(result_reg),.carry_reg(carry_reg));
+ 
+ always 
+    begin
+       #2 clk = ~clk;
+    end
+ initial 
+   begin
+      # 0 clk = 1'b0;  rst = 1'b0;
+      $monitor(" time =%d , x = %b , y = %b, result = %b , carry = %b",$time,x,y,result_reg,carry_reg);
+      #100  rst = 1'b1; x = 8'b11111111; y = 8'b10000000;
+      #100  rst = 1'b1; x = 8'b10000000; y = 8'b10000000;
+      #100  rst = 1'b1; x = 8'b11011111; y = 8'b01100000;
+      #100  rst = 1'b1; x = 8'b11101101; y = 8'b00010000;
+      #100  rst = 1'b1; x = 8'b01110111; y = 8'b01100000;
+      #100  rst = 1'b1; x = 8'b10001111; y = 8'b11111111;
+      #100 $finish;
+   end
+
+endmodule
