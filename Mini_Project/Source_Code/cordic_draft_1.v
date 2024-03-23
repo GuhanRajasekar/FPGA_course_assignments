556f1814d30506fd4951f56c0cd722bd79984184
GuhanRajasekar <149869855+GuhanRajasekar@users.noreply.github.com>
2024-03-23 16:09:12 +0530

    Updated cordic draft 1

--- Mini_Project/Source_Code/cordic_draft_1.v
+++ Mini_Project/Source_Code/cordic_draft_1.v
@@ -4,11 +4,11 @@ module cordic(target_angle,x_res,y_res);
 // However, let us see if this works for simulation
 
  input signed [19:0] target_angle;   // 20 bits to denote the target angle of rotation (16 bits including MSB for the integer part and 4 bits for the fractional part)
- output reg signed [5:0] x_res; // 6 bits (2 for integer part and 16 for fractional part) to store the x-coordinate of the final rotated vector
- output reg signed [5:0] y_res; // 6 bits (2 for integer part and 16 for fractional part) to store the y-coordinate of the final rotated vector
+ output reg signed [19:0] x_res; // 6 bits (2 for integer part and 16 for fractional part) to store the x-coordinate of the final rotated vector
+ output reg signed [19:0] y_res; // 6 bits (2 for integer part and 16 for fractional part) to store the y-coordinate of the final rotated vector
  
- reg signed [5:0] x [9:0];  // to store the x-coordinates of each step (2 bits for the integer part including MSB and 16 bits for the fractional part)
- reg signed [5:0] y [9:0];  // to store the y-coordinates of each step (2 bits for the integer part including MSB and 16 bits for the fractional part)
+ reg signed [19:0] x [9:0];  // to store the x-coordinates of each step (2 bits for the integer part including MSB and 16 bits for the fractional part)
+ reg signed [19:0] y [9:0];  // to store the y-coordinates of each step (2 bits for the integer part including MSB and 16 bits for the fractional part)
  
  wire signed[19:0] arc_tan [9:0];       // to store look up values of arc tan using 20 bits (16 bits including MSB to denote the integer part and 4 bits to denote the fractional part)
  reg  signed[19:0] rotation_angle[9:0];  // Array of 20 bit registers to store the rotated angle 
@@ -30,8 +30,8 @@ module cordic(target_angle,x_res,y_res);
    begin
 // iteration 0:
       // taking k (scaling factor) as 0.6072
-      x[0] = 6'b001001; // x[0] = 0.6072 (2 bits for the integer part and 10 bits for the fractional part)
-      y[0] = 6'b000000; // y[0] = 0.0000 (2 bits for the integer part and 10 bits for the fractional part)
+      x[0] = 20'b00001001101101110001; // x[0] = 0.6072 (2 bits for the integer part and 10 bits for the fractional part)
+      y[0] = 20'b00000000000000000000; // y[0] = 0.0000 (2 bits for the integer part and 10 bits for the fractional part)
       rotation_angle[0] = arc_tan[0]; // First we always rotate by 0 degrees => Update the rotated angle accordingly
       /* For the first time, rotation direction is always 1 assuming the target angle is between 0 and 90 */
       rotation_direction[0] = (rotation_angle[0] < target_angle) ? 1 : 0 ;
@@ -54,16 +54,50 @@ module cordic(target_angle,x_res,y_res);
       
 //iteration 3
       x[3] = (rotation_direction[2] == 1) ? (x[2] - (y[2]>>>2)) : (x[2] + (y[2]>>>2));
-      y[3] = (rotation_direction[2] == 1) ? (y[2] + (x[2]>>>2)) : (y[2] + (x[2]>>>2));
+      y[3] = (rotation_direction[2] == 1) ? (y[2] + (x[2]>>>2)) : (y[2] - (x[2]>>>2));
       rotation_angle[3]     = (rotation_direction[2] == 1) ? (rotation_angle[2] + arc_tan[3]) : (rotation_angle[2] - arc_tan[3]);
       rotation_direction[3] = (rotation_angle[3] < target_angle) ? 1:0;
+
 //iteration 4
+      x[4] = (rotation_direction[3] == 1) ? (x[3] - (y[3]>>>3)) : (x[3] + (y[3]>>>3));
+      y[4] = (rotation_direction[3] == 1) ? (y[3] + (x[3]>>>3)) : (y[3] - (x[3]>>>3));
+      rotation_angle[4]      = (rotation_direction[3] == 1) ? (rotation_angle[3] + arc_tan[4]) : (rotation_angle[3] - arc_tan[4]);
+      rotation_direction[4]  = (rotation_angle[4] < target_angle) ? 1:0;
+       
 //iteration 5
+      x[5] = (rotation_direction[4] == 1) ? (x[4] - (y[4]>>>4)) : (x[4] + (y[4]>>>4));
+      y[5] = (rotation_direction[4] == 1) ? (y[4] + (x[4]>>>4)) : (y[4] - (x[4]>>>4));
+      rotation_angle[5]      = (rotation_direction[4] == 1) ? (rotation_angle[4] + arc_tan[5]) : (rotation_angle[4] - arc_tan[5]);
+      rotation_direction[5]  = (rotation_angle[5] < target_angle) ? 1:0;
+
 //iteration 6
+      x[6] = (rotation_direction[5] == 1) ? (x[5] - (y[5]>>>5)) : (x[5] + (y[5]>>>5));
+      y[6] = (rotation_direction[5] == 1) ? (y[5] + (x[5]>>>5)) : (y[5] - (x[5]>>>5));
+      rotation_angle[6]      = (rotation_direction[5] == 1) ? (rotation_angle[5] + arc_tan[6]) : (rotation_angle[5] - arc_tan[6]);
+      rotation_direction[6]  = (rotation_angle[6] < target_angle) ? 1:0;
+
 //iteration 7
-//iteration 8
-//iteration 9
-//iteration 10
+      x[7] = (rotation_direction[6] == 1) ? (x[6] - (y[6]>>>6)) : (x[6] + (y[6]>>>6));
+      y[7] = (rotation_direction[6] == 1) ? (y[6] + (x[6]>>>6)) : (y[6] - (x[6]>>>6));
+      rotation_angle[7]      = (rotation_direction[6] == 1) ? (rotation_angle[6] + arc_tan[7]) : (rotation_angle[6] - arc_tan[7]);
+      rotation_direction[7]  = (rotation_angle[7] < target_angle) ? 1:0;
       
+//iteration 8
+      x[8] = (rotation_direction[7] == 1) ? (x[7] - (y[7]>>>7)) : (x[7] + (y[7]>>>7));
+      y[8] = (rotation_direction[7] == 1) ? (y[7] + (x[7]>>>7)) : (y[7] - (x[7]>>>7));
+      rotation_angle[8]      = (rotation_direction[7] == 1) ? (rotation_angle[7] + arc_tan[8]) : (rotation_angle[7] - arc_tan[8]);
+      rotation_direction[8]  = (rotation_angle[7] < target_angle) ? 1:0;
+      
+//iteration 9
+      x[9] = (rotation_direction[8] == 1) ? (x[8] - (y[8]>>>8)) : (x[8] + (y[8]>>>8));
+      y[9] = (rotation_direction[8] == 1) ? (y[8] + (x[8]>>>8)) : (y[8] - (x[8]>>>8));
+      rotation_angle[9]      = (rotation_direction[8] == 1) ? (rotation_angle[8] + arc_tan[9]) : (rotation_angle[8] - arc_tan[9]);
+      rotation_direction[9]  = (rotation_angle[8] < target_angle) ? 1:0;    
+   end
+   
+always@(*)
+   begin
+      x_res [19:0] = x[9][19:0];
+      y_res [19:0] = y[9][19:0];
    end
 endmodule
